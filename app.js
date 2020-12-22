13f28a3053a3bc8fc0a4e39ca314fca04c952f71
hera <hera@heras-iMac.local>
2020-12-22 13:58:01 -0600

    stuck on getting wiki data to create array for dropdown select

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 const button = document.querySelector('#search');
 const form = document.querySelector("#search-bar")
 const textInput = document.querySelector("#search-bar").value
+let countryCodes = []
+let countryNames = []
+
+async function fetchCodes() {
+  //https://www.mediawiki.org/wiki/API:Parsing_wikitext#GET_request
+  let url = "https://en.wikipedia.org/w/api.php?" +
+    new URLSearchParams({
+      origin: "*",
+      // action: "parse",
+      page: "ISO 3166-1 alpha-2",
+      section: '6',
+      // format: "html",
+  });
+
+  try {
+    const response = await axios.get(url)
+    const data = response.data
+    // renderScape(data)
+    let document = data
+    let table = document.querySelector('table')
+    // let sectionHTML = sectionCodes.
+    console.log(data)
+    console.log(table)
+    // console.log(sectionHTML)
+  } catch (error) {
+    console.log(error)
+  }
+}
+let codes = fetchCodes()
+// let textinner = codes.parse.text
+// let tables = codes.querySelectAll('a')
+// console.log(textinner)
 
 async function fetchImage(countryCode) {
-  const url = `https://api.gbif.org/v1/occurrence/search?limit=1&country=${countryCode}&mediaType=StillImage&taxonKey=212`;
+  let url = `https://api.gbif.org/v1/occurrence/search?limit=1&country=${countryCode}&mediaType=StillImage&taxonKey=212`;
   try {
     const response = await axios.get(url)
     const data = response.data
@@ -14,7 +46,7 @@ async function fetchImage(countryCode) {
   }
 }
 async function fetchSound(countryCode) {
-  const url = `https://api.gbif.org/v1/occurrence/search?limit=1&country=${countryCode}&mediaType=Sound&taxonKey=212`;
+  let url = `https://api.gbif.org/v1/occurrence/search?limit=1&country=${countryCode}&mediaType=Sound&taxonKey=212`;
   try {
     const response = await axios.get(url)
     const data = response.data
